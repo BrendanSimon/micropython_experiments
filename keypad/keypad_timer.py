e07de16809fcff7c9cbdaf8f11ad194be925e3ae
Brendan Simon <brendan.simon@etrix.com.au>
2019-07-25 10:14:09 +1000

    [keypad_timer] Rework `timer_callback()`, restructre/reformat code

--- keypad/keypad_timer.py
+++ keypad/keypad_timer.py
@@ -5,13 +5,14 @@ Keypad module/class for MicroPython, using a timer callback (interrupt)
 Notes
 -----
 
-    * Timer callbacks do not any memory to be allocated on the heap.
+    * Timer callbacks do not allow any memory to be allocated on the heap.
         - `for x in [list]` loop can NOT be used as an iterator object is allocated.
+           NOTE: may not be true for newer versions of MicroPython !!
         - `for x in range(y)` is ok.
 
     * To run type:
-        >>> import keypad_timer as k
-        >>> k.run()
+        >>> import keypad_timer
+        >>> keypad_timer.run()
 
 Testing
 -------
@@ -30,47 +31,49 @@ To Do
     * Refactor into a common base class.
     * Unit tests :-/
     * Video the keypad working :)
+    * Fix bugs :)
 
 """
 
-##============================================================================
+#!============================================================================
 
 import micropython
 
 try:
     from hwconfig import Pin
-except ImportError:
+except ImportError :
     from pyb import Pin
 
 try:
     from hwconfig import Timer
-except ImportError:
+except ImportError :
     from pyb import Timer
 
 try:
     from hwconfig import delay
-except ImportError:
+except ImportError :
     from pyb import delay
 
-##============================================================================
+#!============================================================================
 
-class Keypad_Timer():
-    """Class to scan a Keypad matrix (e.g. 16-keys as 4x4 matrix) and report
-       last key press.
+class Keypad_Timer () :
+    """
+    Class to scan a Keypad matrix (e.g. 16-keys as 4x4 matrix) and report
+    last key press.
     """
 
-    ## Key states
-    KEY_UP      = 0
-    KEY_DOWN    = 1
+    #! Key states
+    KEY_UP      = const( 0 )
+    KEY_DOWN    = const( 1 )
 
     #-------------------------------------------------------------------------
 
-    def __init__(self):
+    def __init__ ( self ) :
         self.init()
 
     #-------------------------------------------------------------------------
 
-    def init(self):
+    def init ( self ) :
         """Initialise/Reinitialise the instance."""
 
         keys = [
@@ -80,25 +83,25 @@ class Keypad_Timer():
                 '*', '0', '#', 'D',
                ]
 
-        ## Initialise all keys to the UP state.
+        #! Initialise all keys to the UP state.
         self.keys = [ { 'char' : key, 'state' : self.KEY_UP } for key in keys ]
 
-        ## Pin names for rows and columns.
+        #! Pin names for rows and columns.
         self.rows = [ 'PD1', 'PD3', 'PD5', 'PD7' ]
         self.cols = [ 'PD9', 'PD11', 'PD13', 'PD15' ]
 
-        ## Pin names for rows and columns.
+        #! Pin names for rows and columns.
         self.rows = [ 'PD1', 'PD3', 'PD5', 'PD7' ]
         self.cols = [ 'PD9', 'PD11', 'PD13', 'PD15' ]
 
-        ## Initialise row pins as outputs.
+        #! Initialise row pins as outputs.
         self.row_pins = [ Pin(pin_name, mode=Pin.OUT) for pin_name in self.rows ]
 
-        ## Initialise column pins as inputs.
+        #! Initialise column pins as inputs.
         self.col_pins = [ Pin(pin_name, mode=Pin.IN, pull=Pin.PULL_DOWN) for pin_name in self.cols ]
 
-        self.timer = Timer(5, freq=100)
-        self.timer.callback(None)
+        self.timer = Timer( 5, freq=100 )
+        self.timer.callback( None )
 
         self.scan_row = 0
         self.key_code = None
@@ -106,119 +109,125 @@ class Keypad_Timer():
 
     #-------------------------------------------------------------------------
 
-    def get_key(self):
+    def get_key ( self ) :
         """Get last key pressed."""
 
         key_char = self.key_char
 
-        self.key_code = None    ## consume last key pressed
-        self.key_char = None    ## consume last key pressed
+        self.key_code = None    #! consume last key pressed
+        self.key_char = None    #! consume last key pressed
 
         return key_char
 
     #-------------------------------------------------------------------------
 
-    def key_process(self, key_code, col_pin):
+    def key_process ( self, key_code, col_pin ) :
         """Process a key press or release."""
 
         key_event = None
 
-        if col_pin.value():
-            if self.keys[key_code]['state'] == self.KEY_UP:
+        if col_pin.value() :
+            if self.keys[ key_code ][ 'state' ] == self.KEY_UP :
                 key_event = self.KEY_DOWN
-                self.keys[key_code]['state'] = key_event
+                self.keys[ key_code ][ 'state' ] = key_event
         else:
-            if self.keys[key_code]['state'] == self.KEY_DOWN:
+            if self.keys[ key_code ][ 'state' ] == self.KEY_DOWN :
                 key_event = self.KEY_UP
-                self.keys[key_code]['state'] = key_event
+                self.keys[ key_code ][ 'state' ] = key_event
 
         return key_event
 
     #-------------------------------------------------------------------------
 
     def scan_row_update(self):
-        """Timer interrupt callback to scan next keypad row/column.
-           NOTE: This is a true interrupt and no memory can be allocated !!
+        """
+        Timer interrupt callback to scan next keypad row/column.
+        NOTE: This is a true interrupt and no memory can be allocated !!
         """
 
-        ## Deassert row.
-        self.row_pins[self.scan_row].value(0)
+        #! Deassert row.
+        self.row_pins[ self.scan_row ].value( 0 )
 
-        ## Update row.
-        self.scan_row += 1
-        if self.scan_row >= len(self.row_pins):
-            self.scan_row = 0
+        #! Next scan row.
+        self.scan_row = ( self.scan_row + 1 ) % len( self.row_pins )
 
-        ## Assert next row.
-        self.row_pins[self.scan_row].value(1)
+        #! Assert next row.
+        self.row_pins[ self.scan_row ].value( 1 )
 
     #-------------------------------------------------------------------------
 
-    def timer_callback(self, timer):
-        """Timer interrupt callback to scan next keypad row/column.
-           NOTE: This is a true interrupt and no memory can be allocated !!
+    def timer_callback ( self, timer ) :
+        """
+        Timer interrupt callback to scan next keypad row/column.
+        NOTE: This is a true interrupt and no memory can be allocated !!
         """
 
         #print("DEBUG: Keypad.timer_callback()")
 
-        ## Can't use `for x in [list]` loop in micropython time callback as memory is allocated
-        ## => exception in timer interrupt !!
+        #! Can't use `for x in [list]` loop in micropython time callback as memory is allocated
+        #! => exception in timer interrupt !!
 
-        key_code = self.scan_row * len(self.cols)
-        for col in range(len(self.cols)):
-            ## Process pin state.
-            key_event = self.key_process(key_code + col, self.col_pins[col])
-            ## Process key event.
+        #! key code/index for first column of current row
+        key_code = self.scan_row * len( self.cols )
+
+        for col in range( len( self.cols ) ) :
+            #! Process pin state.
+            key_event = self.key_process( key_code, self.col_pins[ col ] )
+            
+            #! Process key event.
             if key_event == self.KEY_DOWN:
-                self.key_code = key_code + col
-                self.key_char = self.keys[key_code + col]['char']
+                self.key_code = key_code
+                self.key_char = self.keys[ key_code ][ 'char' ]
+
+            #! Next key code (i.e. for next column)
+            key_code += 1
 
         self.scan_row_update()
 
     #-------------------------------------------------------------------------
 
-    def start(self):
+    def start ( self ) :
         """Start the timer."""
 
-        self.timer.callback(self.timer_callback)
+        self.timer.callback( self.timer_callback )
 
     #-------------------------------------------------------------------------
 
-    def stop(self):
+    def stop ( self ) :
         """Stop the timer."""
 
-        self.timer.callback(None)
+        self.timer.callback( None )
 
-##============================================================================
+#!============================================================================
 
-def main_test():
+def main_test () :
     """Main test function."""
 
-    print("main_test(): start")
+    print( "main_test(): start" )
 
-    micropython.alloc_emergency_exception_buf(100)
+    micropython.alloc_emergency_exception_buf( 100 )
 
     keypad = Keypad_Timer()
     keypad.start()
 
-    try:
-        #for i in range(10000):
-        while True:
+    try :
+        #for i in range( 10000 ) :
+        while True :
             key = keypad.get_key()
-            if key:
-                print("keypad: got key:", key)
-            delay(1)
-    except Exception as ex:
-        pass
+            if key :
+                print( "keypad: got key:", key )
+            delay( 1 )
+    except Exception as exc :
+        print( "Exception:", str( exc ) )
 
     keypad.stop()
 
-    print("main_test(): end")
+    print( "main_test(): end" )
 
-##============================================================================
+#!============================================================================
 
 run = main_test
 
-if __name__ == '__main__':
+if __name__ == '__main__' :
     main_test()
 
